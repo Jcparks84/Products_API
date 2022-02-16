11a4de43cf3d071edcc9d223036f78e48ed74956
John Parks <jcparks1984@gmail.com>
2022-02-16 14:31:41 -0500

    Created Function To Update Product In View File

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
+from webbrowser import get
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductsSerializer
 from .models import Products
+from products import serializers
 
 
 @api_view(['GET', 'POST'])
@@ -18,3 +21,16 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT','DELETE'])
+def products_detail(request, pk):
+    products = get_object_or_404(Products, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductsSerializer(products)
+        return Response(serializer.data)
+    elif request.method =='PUT':
+        serializer = ProductsSerializer(products, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
